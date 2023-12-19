cd848dd2e5c9a3d6614f53da6e77a0b591b169f0
Mohamed AboubakeElSedik Hassan <mhassan@ejadaalex.local>
2023-12-19 16:17:57 +0200

    update encoding

--- encoding-library/vars/encode.groovy
+++ encoding-library/vars/encode.groovy
@@ -2,5 +2,5 @@ import java.util.Base64;
 
 def encode(String message) {
     Base64.Encoder encoder = Base64.getEncoder();
-    return encoder.encodeToString(message.getBytes())
+    return encoder.encodeToString(message.getBytes('UTF-8'))
 }
